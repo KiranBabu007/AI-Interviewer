b13cbbfa53c196cf79310fe8a959f94dc9fd3860
Sidharth-Manikuttan <sidharthmanikuttan9@gmail.com>
2024-12-15 15:13:47 +0530

    About and How It Works Sections

---
+++ my-app/app/About/layout.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+
+import Navbar from '../../components/Navbar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
