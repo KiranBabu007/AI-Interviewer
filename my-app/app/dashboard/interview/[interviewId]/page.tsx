a78d791fb8add64091729865fa58d34f133d99f9
KiranBabu007 <kiranbabu3355@gmail.com>
2024-12-15 14:36:28 +0530

    feat: implement interview details fetching with error handling for invalid interviewId

--- my-app/app/dashboard/interview/[interviewId]/page.tsx
+++ my-app/app/dashboard/interview/[interviewId]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { db } from '@/utils/db';
 import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
@@ -5,11 +6,21 @@ import { useParams } from 'next/navigation';
 import React, { useEffect } from 'react'
 
 const page = () => {
+
+  const [interviewDetails, setInterviewDetails]= React.useState<any>(null);
+  const [questions, setQuestions]= React.useState<any>(null);
+
   useEffect(() => {
+    GetInterviewDetails();
   },[]);
-  const interviewId= useParams().interviewId
-  const GetInterviewDetails= async () => {
-    const result= await db.select().from(MockInterview).where(eq(MockInterview.mockId, interviewId));
+  const interviewId = useParams().interviewId;
+  const GetInterviewDetails = async () => {
+    if (typeof interviewId === 'string') {
+      const result = await db.select().from(MockInterview).where(eq(MockInterview.mockId, interviewId));
+      console.log(result);
+    } else {
+      console.error('Invalid interviewId');
+    }
   }
   return (
     <div>
