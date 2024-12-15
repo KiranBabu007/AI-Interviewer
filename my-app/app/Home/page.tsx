6153340721f17ae9036f65349d10bc27d3875b00
Indrajiths4 <indrajithmundackal@gmail.com>
2024-12-15 16:28:50 +0530

    landing

--- my-app/app/Home/page.tsx
+++ my-app/app/Home/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Landing from '../../components/Landing'
+import AboutUs from '@/components/About'
+import  HowItWorks  from '@/components/HowItWorks'
 
 const page = () => {
   return (
     <div >
-      <Landing />
+      <Landing/>
+      <AboutUs/>
+      <HowItWorks/>
     </div>
   )
 }
