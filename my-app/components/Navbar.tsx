55433a38972c9c34ece01b80555176e319b28b1c
KiranBabu007 <kiranbabu3355@gmail.com>
2024-12-15 21:16:55 +0530

    Fix Navbar layout by adding missing div for improved structure

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -54,6 +54,7 @@ const Navbar = ({
                   />
                 </svg>
               </Link>
+              
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4 font-bold">
