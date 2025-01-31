fe5328816255b6cdc0c6d4cac9f1cfe3752c3f4c
KiranBabu007 <kiranbabu3355@gmail.com>
2025-01-31 16:16:04 +0530

    Add Vercel Speed Insights component and update dependencies

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css'; 
 import { dark } from '@clerk/themes';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body>
           <div>
             {children}
+            <SpeedInsights />
           </div>
         </body>
       </html>
